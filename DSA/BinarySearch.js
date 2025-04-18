912f8c2758df33480547c95266232feba6bad8a2
Saurabh rawat <155474450+saurabhrawat-13@users.noreply.github.com>
2025-04-18 19:39:44 +0000

    Done

---
+++ DSA/BinarySearch.js
@@ -0,0 +1,20 @@
+function BinarySearch(arr, target) {
+    let leftindex = 0
+    let rightindex = arr.length - 1
+
+while (leftindex <= rightindex) {
+    let middleindex = Math.floor((leftindex + rightindex)/2)
+    if(target === arr[middleindex]){
+        return middleindex
+    }
+    if(target < arr[middleindex]){
+        rightindex = middleindex -1 
+    }else{
+        leftindex = middleindex + 1
+    }
+  }
+  return -1
+}
+console.log(BinarySearch([-5, 2, 4,6,10], 10));
+console.log(BinarySearch([-5, 2, 4,6,10], 6));
+console.log(BinarySearch([-5, 2, 4,6,10], 20));
